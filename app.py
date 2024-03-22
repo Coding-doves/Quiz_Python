16109490bdff779e274c3f975296a6347a1a7f38
Ada <obenedicta4.@gmail.com>
2024-03-22 17:49:05 +0100

    added dashboard.html, login.html, signup.html, updated app.py, changed index.html to quiz.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import bcrypt
 import mysql.connector
 from quiz import Quiz
@@ -9,15 +9,65 @@ app = Flask(__name__)
 # Connect to mysql db
 db = mysql.connector.connect(
     host='localhost',
-    user='dove',
-    password='password',
+    user='root',
+    password='Ada.070#X',
     database='QuizApp'
 )
 cursor =db.cursor()
 
 
-@app.route('/')
-def index():
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    """authenticating user """
+    if request.method == 'POST':
+        data = request.form
+        username = data.get('username')
+        passwd = data.get('password')
+
+        query = "SELECT password FROM user WHERE username = %s"
+        cursor.execute(query, (username,))
+        value = cursor.fetchone()
+
+        if value:
+            hashed_password = value[0]
+            if bcrypt.checkpw(passwd.encode('utf-8'), hashed_password.encode('utf-8')):
+                # --- work on session for logged in user
+                return redirect(url_for('quiz'))
+            else:
+                return render_template('login.html', message="Wrong password")
+        else:
+            return render_template('login.html', message="User not found")
+
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    ''' creating a new user '''
+    if request.method == 'POST':
+        data = request.form
+        firstname = data.get('firstname')
+        lastname = data.get('lastname')
+        username = data.get('username')
+        password = data.get('password')
+
+        # Ensure password is at least 6 character
+        if len(password) < 6:
+            return render_template('signup.html', error='Password must be at least six characters long')
+
+        # encrypt the password
+        hashed_passwd = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        query = 'INSERT INTO users (first_name, last_name, username, password) VALUES (%s, %s, %s, %s)'
+        cursor.execute(query, (firstname, lastname, username, hashed_passwd))
+        db.commit()
+
+        return redirect(url_for('login'))
+    
+    return render_template('signup.html')
+
+
+@app.route('/quiz')
+def quiz():
     ''' output to browser '''
     # api endpoint
     api_url = "https://the-trivia-api.com/v2/questions"
@@ -26,7 +76,7 @@ def index():
     quiz = Quiz(api_url)
     random.shuffle(quiz.questions)
     # print("Quiz Questions:", quiz.questions)
-    return render_template('index.html', questions=quiz.questions)
+    return render_template('quiz.html', questions=quiz.questions)
 
 
 @app.route('/submit', methods=['POST'])
@@ -56,6 +106,18 @@ def submit():
         score=score,
         total_questions=total_questions,
         question_data=question_data)
+
+
+@app.route('/logout')
+def logout():
+    ''' end and clear sessions for logged in users '''
+    # -----work on clear the sessions
+    return redirect(url_for('login'))
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 if __name__ == '__main__':
