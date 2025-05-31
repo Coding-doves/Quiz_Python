edc45f79b187bb6be29a5c665f8f1c8d7fc7ca7b
Ada <obenedicta4@gmail.com>
2025-05-31 17:17:40 +0100

    Update db.py

--- app/db.py
+++ app/db.py
@@ -10,10 +10,4 @@ def connect_to_database():
         database=os.getenv("DB_NAME"),
         port=int(os.getenv("DB_PORT", 3306))
     )
-    print("DB_HOST:", os.getenv("DB_HOST"))
-    print("DB_PORT:", os.getenv("DB_PORT"))
-    print("DB_USER:", os.getenv("DB_USER"))
-    print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
-    print("DB_NAME:", os.getenv("DB_NAME"))
-
     return conn
