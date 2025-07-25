9386b09b3d24f5d2b6c81dc4a8aad7cfd2669627
Ada <obenedicta4@gmail.com>
2025-07-25 08:33:53 +0100

    Update db.py
    moved db creation in here

--- app/db.py
+++ app/db.py
@@ -12,3 +12,57 @@ def connect_to_database():
     )
     return conn
     
+
+# Connect to PostgreSQL
+db = connect_to_database()
+cursor = db.cursor()
+
+# Create tables if they don't exist
+cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+)''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS quiz_scores (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    score INT,
+    total_questions INT,
+    quiz_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS quiz_attempted (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    quiz_score_id INT,
+    question VARCHAR(255),
+    user_answer VARCHAR(255),
+    correct_answer VARCHAR(255),
+    quiz_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (quiz_score_id) REFERENCES quiz_scores(id)
+)''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS images (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    profile_image BYTEA,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)''')
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS password_reset_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    reset_token VARCHAR(100) NOT NULL,
+    token_expiry TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)''')
+
+db.commit()
+cursor.close()
+db.close()
