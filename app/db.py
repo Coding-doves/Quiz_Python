655ccf056b9fd10f32adf80a19f29215c2fa0ce3
Ada <obenedicta4@gmail.com>
2025-07-25 08:05:21 +0100

    Update db.py
    change from mysl to postgres

--- app/db.py
+++ app/db.py
@@ -1,13 +1,14 @@
 import os
-import mysql.connector
+import psycopg2
 
 
 def connect_to_database():
-    conn = mysql.connector.connect(
+    conn = psycopg2.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=int(os.getenv("DB_PORT", 3306)),
+        dbname=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 5432)),  # default PostgreSQL port
     )
     return conn
+    
